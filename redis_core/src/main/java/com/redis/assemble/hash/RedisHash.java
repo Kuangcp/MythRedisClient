222b57dc70190d46b175c319b7d649092df44176
mythos <kuangchengping@outlook.com>
2017-06-23 09:24:42 +0800

    Who is master

---
+++ redis_core/src/main/java/com/redis/assemble/hash/RedisHash.java
@@ -0,0 +1,9 @@
+package com.redis.assemble.hash;
+
+import com.redis.common.Commands;
+
+/**
+ * Created by https://github.com/kuangcp on 17-6-23  上午9:24
+ */
+public class RedisHash extends Commands {
+}
